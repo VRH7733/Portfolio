832c804adc7db115fc7961d1f2ac0cb329d3cfad
raghu <raghavendravenkatesh1@gmail.com>
2024-12-16 09:30:21 +0530

    v1

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-vite-app/', // Replace with your repository name
+  base: '/Portfolio/', // Replace with your repository name
 });
